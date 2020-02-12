3f2ee232a3bb1ef7c7f010ff33ef0bc943544e06
Sydney Devaney <Sydney@Sydneys-MacBook-Air.local>
2020-02-12 10:06:02 -0500

    Updates

---
+++ precache-manifest.2a58bf5ad396c0d6dcbf2bcfe05bca56.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1968c892716265bd3b0f14b5c9db5ef",
+    "url": "/dig4639-mobile-dev/index.html"
+  },
+  {
+    "revision": "c6a232d378d410913fed",
+    "url": "/dig4639-mobile-dev/static/css/main.0ebde3ef.chunk.css"
+  },
+  {
+    "revision": "e02cba94be16e740280c",
+    "url": "/dig4639-mobile-dev/static/js/2.3ded2b1a.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/dig4639-mobile-dev/static/js/2.3ded2b1a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6a232d378d410913fed",
+    "url": "/dig4639-mobile-dev/static/js/main.6dda1aa3.chunk.js"
+  },
+  {
+    "revision": "170169ccdc7dff24daa3",
+    "url": "/dig4639-mobile-dev/static/js/runtime-main.e7bb69b4.js"
+  }
+]);
