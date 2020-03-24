775ae8992659a54b0a27f5b33648d8a6979d1584
Sydney Devaney <Sydney@Sydneys-MacBook-Air.local>
2020-03-24 11:03:56 -0400

    w12a commir

--- demo/w12a/sync.js
+++ demo/w12a/sync.js
@@ -2,10 +2,10 @@
  * This file contains examples of code that executes sequentially
  */
 
-function synchronousFunction() {
-  console.log("First we do this.")
-  console.log("Then we do this.")
-  console.log("Finally we celebrate!")
+function synchronousFunction () {
+  console.log('First we do this.')
+  console.log('Then we do this.')
+  console.log('Finally we celebrate!')
 }
 
 synchronousFunction()
